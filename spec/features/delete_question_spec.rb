8475dce119e3e976d2ab5b231cbe05a0710dfa33
Elise Fitzgerald <elise.fitzgerald15@gmail.com>
2015-06-24 17:32:48 -0400

    added feature to delete question and associated answers

--- spec/features/delete_question_spec.rb
+++ spec/features/delete_question_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 # Acceptance Criteria
-# [ ]- I must be able delete a question from the question edit page
+# [✓]- I must be able delete a question from the question edit page
 # [✓] - I must be able delete a question from the question details page
-# [ ] - All answers associated with the question must also be deleted
+# [✓] - All answers associated with the question must also be deleted
 
 feature "As a user
 I want to delete a question
